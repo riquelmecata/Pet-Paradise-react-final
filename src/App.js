b089abf2ad4c14431056c6dadf42bd874b036577
Catalina Riquelme <riquelmecata@gmail.com>
2023-11-28 21:02:29 -0300

    listo

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import "./main.css";
 import { NavBar } from './components/NavBar/NavBar';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Cambio a HashRouter
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartProvider } from './context/CartContext';
-import { Cart } from './components/Cart/Cart';
+import { Cart } from './components/Cart/Cart'
 import Checkout from './components/Checkout/Checkout';
+
 
 function App() {
   return (
     <div className="App">
-      <Router basename="/Pet-Paradise-react-final"> {/* Configuración del basename */}
+      <BrowserRouter basename="/Pet-Paradise-react-final">
         <CartProvider>
           <NavBar />
           <Routes>
-            <Route path='/' element={<ItemListContainer />} />
-            <Route path='/productos/:categoria' element={<ItemListContainer />} />
-            <Route path='/item/:itemId' element={<ItemDetailContainer />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/checkout' element={<Checkout />} />
-            <Route path='*' element={<h1> Página no encontrada </h1>} />
+            <Route path='/' element={<ItemListContainer /> }/>
+            <Route path='/productos/:categoria' element={<ItemListContainer /> }/>
+            <Route path='/item/:itemId' element={<ItemDetailContainer /> }/>
+            <Route path='/cart' element={<Cart /> }/>
+            <Route path='/checkout' element={<Checkout /> }/>
+            <Route path='*' element={<h1> Página no encontrada </h1> }/>
           </Routes>
         </CartProvider>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
